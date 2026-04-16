46366ca61d360097e54468c97a36d1517ace3cf9
ragnar_harsh <harshsingh8118@gmail.com>
2026-04-16 19:17:42 +0530

    Finished Model

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
+import pickle
+import numpy as np
 
 
 
 app = Flask(__name__)
 
 cars = pd.read_csv('./Datasets/Cleaned_Car_Data.csv')
+predict_model = pickle.load(open('./Notebook/LinearRegressionModel.pkl', 'rb'))
 
 
 
@@ -20,6 +23,24 @@ def index():
     fuel_type = cars['fuel_type'].unique()
 
     return render_template('index.html', company_list = companies, car_model_list = car_models, year_list = year, fuel_type_list = fuel_type)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    company = request.form.get('company')
+    model = request.form.get('model')
+    year = int(request.form.get('year'))
+    fuel = request.form.get('fuel')
+    km_driven = int(request.form.get('kms_driven'))
+
+    est_price = predict_model.predict(pd.DataFrame([[model, company, year, km_driven, fuel]], columns=['name', 'company', 'year', 'kms_driven', 'fuel_type']))
+
+
+
+    return str(np.round(est_price[0], 2))
+    
+
 
 
 if __name__ == '__main__':
